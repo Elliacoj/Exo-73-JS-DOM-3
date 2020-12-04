b240b3bac3ea3910542b66a6e0da23e388a05db6
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-04 10:08:26 +0100

    Modif

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,9 @@
 let avatarEls = document.getElementsByTagName("img");
 
-
+for(let i = 0; i < avatarEls.length; i++) {
+    avatarEls[i].style.height = "200px";
+    avatarEls[i].title = avatarEls[i].alt;
+}
 
 
 
--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,4 +1,14 @@
+let div1 = document.createElement("div");
+let div2 = document.createElement("div")
 
+div1.className = "planet";
+div1.style.backgroundColor = "blue";
+
+div2.className = "moon";
+div2.style.backgroundColor = "green";
+
+document.body.append(div1);
+div1.appendChild(div2);
 
 
 
